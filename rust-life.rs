24cf6ddc6c21858c62efd404470295a49b1bb4f1
Mike MacDonald <crazymykl@gmail.com>
2014-07-15 14:44:42 -0400

    make it work with latest master, again

--- rust-life.rs
+++ rust-life.rs
@@ -185,7 +185,7 @@ fn testing_board(n: uint) -> Board {
 
 #[test]
 fn test_board_str_conversion() {
-  assert_eq!(testing_board(0).to_str(), test_boards[0].to_string());
+  assert_eq!(testing_board(0).to_string(), test_boards[0].to_string());
 }
 
 #[test]
