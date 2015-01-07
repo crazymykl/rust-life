869d0c85bbb6eda3b92c78b6508cc672cbcd95fe
Mike MacDonald <crazymykl@gmail.com>
2015-01-07 01:02:18 -0500

    Make sure the clock didn't hang up somehow.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
   let periodic = timer.periodic(std::time::Duration::milliseconds(64));
   loop {
     println!("\x1b[H\x1b[2J{}", brd);
-    periodic.recv();
+    periodic.recv().unwrap();
     brd = brd.parallel_next_generation(worker_pool);
   }
 }
