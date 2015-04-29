8fbf99014517b5cc2ef703bd5183740506c90823
Mike MacDonald (crazymykl) <crazymykl@gmail.com>
2015-04-29 08:26:38 -0400

    Fix build. Use snooze.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-#![feature(std_misc, test, old_io)]
+#![feature(std_misc, test)]
 
 extern crate threadpool;
 extern crate rand;
 extern crate num_cpus;
+extern crate snooze;
+
+use std::time::duration::Duration;
 
 mod board;
 
 #[cfg(not(test))]
 fn main() {
+  let mut snooze = snooze::Snooze::new(Duration::milliseconds(64)).unwrap();
   let mut brd = board::Board::new(65, 248).random();
-  let mut timer = std::old_io::Timer::new().unwrap();
   let ref mut worker_pool = board::WorkerPool::new_with_default_size();
-  let periodic = timer.periodic(std::time::Duration::milliseconds(64));
   loop {
     println!("\x1b[H\x1b[2J{}", brd);
-    periodic.recv().unwrap();
+    let _ = snooze.wait();
     brd = brd.parallel_next_generation(worker_pool);
   }
 }
