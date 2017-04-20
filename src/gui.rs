54226a713422be931ecd330c1fbb45f5631a8c51
Nathan Lilienthal <nathan@nixpulvis.com>
2017-04-19 23:53:20 -0700

    Some reformatting.

--- src/gui.rs
+++ src/gui.rs
@@ -14,7 +14,8 @@ fn scale_dimension(x: u32) -> usize {
 
 pub fn main() {
     let (rows, cols) = (scale_dimension(X_SZ), scale_dimension(Y_SZ));
-    let mut brd = board::Board::new(rows, cols).random();
+    let mut board = board::Board::new(rows, cols).random();
+    // let ref mut worker_pool = board::WorkerPool::new_with_default_size();
 
     let mut window: PistonWindow = WindowSettings::new("Life", [X_SZ, Y_SZ])
         .exit_on_esc(true)
@@ -30,41 +31,40 @@ pub fn main() {
         &TextureSettings::new()
     ).unwrap();
 
-    while let Some(e) = window.next() {
-        e.mouse_cursor(|x, y| {
+    while let Some(event) = window.next() {
+        event.mouse_cursor(|x, y| {
             cursor = [x as u32, y as u32];
         });
 
-        if let Some(btn) = e.press_args() {
-            match btn {
+        if let Some(button) = event.press_args() {
+            match button {
                 Button::Mouse(MouseButton::Left) => {
-                    let (x, y) = (scale_dimension(cursor[0]) - 1
-                                , scale_dimension(cursor[1]) - 1);
-                    brd = brd.toggle(x, y);
+                    let (x, y) = (scale_dimension(cursor[0]) - 1,
+                                  scale_dimension(cursor[1]) - 1);
+                    board = board.toggle(x, y);
                 },
-                Button::Mouse(MouseButton::Right)
-                | Button::Keyboard(Key::Space)   => running = !running,
-                Button::Keyboard(Key::C)         => brd = brd.clear(),
-                Button::Keyboard(Key::R)         => brd = brd.random(),
-                Button::Keyboard(Key::S)         => brd = brd.parallel_next_generation(),
-                _                                => {}
+                Button::Keyboard(Key::Space) => running = !running,
+                Button::Keyboard(Key::C) => board = board.clear(),
+                Button::Keyboard(Key::R) => board = board.random(),
+                Button::Keyboard(Key::S) => board = board.parallel_next_generation(),
+                _ => {}
             };
         }
 
-        if let Some(_) = e.render_args() {
-            for (x, y, val) in brd.cells() {
+        if let Some(_) = event.render_args() {
+            for (x, y, val) in board.cells() {
                 let color = if val { [255, 255, 255, 255] } else { [0, 0, 0, 255] };
                 canvas.put_pixel(y as u32, x as u32, im::Rgba(color));
             }
             texture.update(&mut window.encoder, &canvas).unwrap();
-            window.draw_2d(&e, |c, g| {
+            window.draw_2d(&event, |c, g| {
                 clear([0.0; 4], g);
                 image(&texture, c.transform.scale(SCALE, SCALE), g);
             });
         }
 
-        if let Some(_) = e.update_args() {
-            if running { brd = brd.parallel_next_generation(); }
+        if let Some(_) = event.update_args() {
+            if running { board = board.parallel_next_generation(); }
         }
     }
 }
