9d4ca2d860fc2e422646087b56529ff186f54e9c
Mike MacDonald <crazymykl@gmail.com>
2017-04-24 10:23:07 -0400

    Avoid extra allocations. v0.2.2

--- src/board.rs
+++ src/board.rs
@@ -50,7 +50,7 @@ impl Board {
     }
 
     pub fn random(&self) -> Board {
-        let brd = thread_rng().gen_iter::<bool>().take(self.len()).collect();
+        let brd = thread_rng().gen_iter().take(self.len()).collect();
 
         self.next_board(brd)
     }
@@ -64,9 +64,8 @@ impl Board {
 
     pub fn parallel_next_generation(&self) -> Board {
         let new_brd = (0..self.len())
-            .collect::<Vec<usize>>()
-            .par_iter()
-            .map(|&x| self.successor_cell(x))
+            .into_par_iter()
+            .map(|cell| self.successor_cell(cell))
             .collect();
 
         self.next_board(new_brd)
