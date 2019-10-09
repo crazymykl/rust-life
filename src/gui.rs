54413181de65647099365f930498c84d9497e7fb
Nathan Lilienthal <nathan@nixpulvis.com>
2019-10-09 00:51:19 -0400

    Proper fullscreen.

--- src/gui.rs
+++ src/gui.rs
@@ -9,23 +9,27 @@ use board;
 // const X_SZ: u32 = 1920;
 // const Y_SZ: u32 = 1080;
 
-const SCALE: u32 = 10;
-const X_SZ: u32 = 800;
-const Y_SZ: u32 = 600;
+const SCALE: u32 = 5;
+const X_SZ: u32 = 1920;
+const Y_SZ: u32 = 1080;
+
+// const SCALE: u32 = 10;
+// const X_SZ: u32 = 1920;
+// const Y_SZ: u32 = 1080;
+
+// const SCALE: u32 = 3;
+// const X_SZ: u32 = 800;
+// const Y_SZ: u32 = 600;
 
 pub fn main() {
     let (rows, cols) = (X_SZ / SCALE, Y_SZ / SCALE);
     let mut brd = board::Board::new(rows as usize, cols as usize).random();
-    // let ref mut worker_pool = board::WorkerPool::new_with_default_size();
-
     let mut window: PistonWindow = WindowSettings::new("Life", [X_SZ, Y_SZ])
         .exit_on_esc(true)
         .opengl(OpenGL::V3_2)
         .fullscreen(true)
         .build()
         .unwrap();
-    let mut running = true;
-    let mut cursor = [0, 0];
     let mut canvas = im::ImageBuffer::new(X_SZ as u32, X_SZ as u32);
     let mut texture = Texture::from_image(
         &mut window.factory,
@@ -33,6 +37,8 @@ pub fn main() {
         &TextureSettings::new()
     ).unwrap();
 
+    let mut running = true;
+    let mut cursor = [0, 0];
     while let Some(e) = window.next() {
         e.mouse_cursor(|x, y| {
             cursor = [x as u32, y as u32];
@@ -41,9 +47,9 @@ pub fn main() {
         if let Some(btn) = e.press_args() {
             match btn {
                 Button::Mouse(MouseButton::Left) => {
-                    // let (x, y) = (scale_dimension(cursor[0]) - 1,
-                    //               scale_dimension(cursor[1]) - 1);
-                    // brd = brd.toggle(x, y);
+                    let (x, y) = (cursor[0] / SCALE,
+                                  cursor[1] / SCALE);
+                    brd = brd.toggle(x as usize, y as usize);
                 },
                 Button::Mouse(MouseButton::Right)
                 | Button::Keyboard(Key::Space)   => running = !running,
@@ -54,15 +60,23 @@ pub fn main() {
             };
         }
 
-        if e.render_args().is_some() {
+        if let Some(_) = e.render_args() {
             for (x, y, val) in brd.cells() {
-                let color = if val { [255, 255, 255, 255] } else { [0, 0, 0, 255] };
+                let color = if val {
+                    [255, 255, 255, 255]
+                } else {
+                    [0, 0, 0, 255]
+                };
+
                 for i in (0..SCALE) {
-                    let sx = x as u32 * SCALE + i;
-                    let sy = y as u32 * SCALE + i;
-                    canvas.put_pixel(sy, sx, im::Rgba(color));
+                    for j in (0..SCALE) {
+                        let sx = x as u32 * SCALE + i;
+                        let sy = y as u32 * SCALE + j;
+                        canvas.put_pixel(sy, sx, im::Rgba(color));
+                    }
                 }
             }
+
             texture.update(&mut window.encoder, &canvas).unwrap();
             window.draw_2d(&e, |c, g| {
                 clear([0.0; 4], g);
@@ -70,8 +84,10 @@ pub fn main() {
             });
         }
 
-        if e.update_args().is_some() && running {
-            brd = brd.parallel_next_generation();
+        if let Some(_) = e.update_args() {
+            if running {
+                brd = brd.parallel_next_generation();
+            }
         }
     }
 }
