aae420b82ffab2419c5b38045df487a4d8270efb
Nathan Lilienthal <nathan@nixpulvis.com>
2019-10-09 02:06:39 -0400

    Set updates per second.

--- src/gui.rs
+++ src/gui.rs
@@ -4,22 +4,29 @@ extern crate image as im;
 use self::piston_window::*;
 use board;
 
-// woh
-// const SCALE: f64 = 1.0;
+// // Like my AVR build.
+// const UPS: u32 = 1;
+// const SCALE: u32 = 10;
+// const X_SZ: u32 = 80;
+// const Y_SZ: u32 = 80;
+
+// // Like my ARM build.
+// const UPS: u32 = 15;
+// const SCALE: u32 = 5;
+// const X_SZ: u32 = 64*5;
+// const Y_SZ: u32 = 64*5;
+
+// // But now we have real hardware.
+// const UPS: u32 = 10;
+// const SCALE: u32 = 4;
 // const X_SZ: u32 = 1920;
 // const Y_SZ: u32 = 1080;
 
-const SCALE: u32 = 5;
+// This is a pretty big bang.
+const UPS: u32 = 120;
+const SCALE: u32 = 1;
 const X_SZ: u32 = 1920;
 const Y_SZ: u32 = 1080;
-
-// const SCALE: u32 = 10;
-// const X_SZ: u32 = 1920;
-// const Y_SZ: u32 = 1080;
-
-// const SCALE: u32 = 3;
-// const X_SZ: u32 = 800;
-// const Y_SZ: u32 = 600;
 
 pub fn main() {
     let (rows, cols) = (X_SZ / SCALE, Y_SZ / SCALE);
@@ -30,6 +37,14 @@ pub fn main() {
         .fullscreen(true)
         .build()
         .unwrap();
+    window.events = Events::new(EventSettings {
+        max_fps: 60,
+        ups: UPS as u64,
+        ups_reset: 2,
+        swap_buffers: true,
+        lazy: false,
+        bench_mode: false,
+    });
     let mut canvas = im::ImageBuffer::new(X_SZ as u32, X_SZ as u32);
     let mut texture = Texture::from_image(
         &mut window.factory,
@@ -68,8 +83,8 @@ pub fn main() {
                     [0, 0, 0, 255]
                 };
 
-                for i in (0..SCALE) {
-                    for j in (0..SCALE) {
+                for i in 0..SCALE {
+                    for j in 0..SCALE {
                         let sx = x as u32 * SCALE + i;
                         let sy = y as u32 * SCALE + j;
                         canvas.put_pixel(sy, sx, im::Rgba(color));
