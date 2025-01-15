c1248c5956b5791b2bb07672fae1e4bfb7428f58
Mike MacDonald <crazymykl@gmail.com>
2025-01-15 15:51:18 -0500

    Bump deps

--- src/benchmarks.rs
+++ src/benchmarks.rs
@@ -5,19 +5,27 @@ use board::Board;
 
 #[bench]
 fn bench_random(b: &mut Bencher) {
-    let brd = Board::new(200,200);
+    let brd = Board::new(200, 200);
     b.iter(|| brd.random());
 }
 
 #[bench]
 fn bench_ten_generations(b: &mut Bencher) {
-    let mut brd = Board::new(200,200).random();
-    b.iter(|| for _ in 0..10 { brd = brd.next_generation(); });
+    let mut brd = Board::new(200, 200).random();
+    b.iter(|| {
+        for _ in 0..10 {
+            brd = brd.next_generation();
+        }
+    });
 }
 
 #[bench]
 fn bench_ten_parallel_generations(b: &mut Bencher) {
-    let mut brd = Board::new(200,200).random();
+    let mut brd = Board::new(200, 200).random();
 
-    b.iter(|| for _ in 0..10 { brd = brd.parallel_next_generation(); });
+    b.iter(|| {
+        for _ in 0..10 {
+            brd = brd.parallel_next_generation();
+        }
+    });
 }
